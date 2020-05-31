87fe22d02d4016884fbbf74ebbb72f9d10dcb5ed
truonghh99 <truonghh@beloit.edu>
2020-05-31 12:12:18 -0700

    Add android version

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,31 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
-    private Rigidbody rb;
-	public float speed;
-    private int count;
     public Text countText;
     public Text winText;
+	public float speed;
+    public Button resetButton;
+
+    private Rigidbody rb;
+    private int count;
+    private GameObject[] pickups;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        pickups = GameObject.FindGameObjectsWithTag("Pick Up");
+
         count = 0;
         SetCountText();
         winText.text = "";
+        resetButton.onClick.AddListener(resetGame);
     }
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-        float moveVertical = Input.GetAxis ("Vertical");
+        float moveHorizontal = Input.acceleration.x;
+        float moveVertical = Input.acceleration.y;
  
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
  
@@ -42,5 +49,28 @@ public class PlayerController : MonoBehaviour {
         if (count >= 15) {
             winText.text = "You won!!!";
         }
+    }
+
+    void resetGame() {
+        resetPickups();
+        resetPosition();
+        resetCount();
+    }
+
+    void resetCount() {
+        count = 0;
+        countText.text = "Count: 0";
+        winText.text = "";
+    }
+
+    void resetPickups() {
+        foreach (GameObject pickup in pickups) {
+            pickup.SetActive(true);
+        }
+    }
+
+    void resetPosition() {
+        Vector3 original = new Vector3 (0.19f, 0.5f, -6.29f);
+        transform.position = original;
     }
 }
