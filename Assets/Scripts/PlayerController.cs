77785a7f5d2d5efca411bbed658c6b75852169c7
truonghh99 <truonghh@beloit.edu>
2020-05-30 18:05:03 -0700

    Add collectable objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
- 
+using UnityEngine.UI;
+
 public class PlayerController : MonoBehaviour {
+
     private Rigidbody rb;
 	public float speed;
+    private int count;
+    public Text countText;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
     }
+
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -16,5 +24,18 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
  
         rb.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up")) {
+            other.gameObject.SetActive(false);
+            ++count;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
     }
 }
